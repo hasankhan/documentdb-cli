a4888ecc7c365b9e0fddd57bddc0cb8e501b4bfa
Muhammad Hasan Khan <slashdot10@gmail.com>
2016-11-19 16:17:29 -0800

    #1 complete tests for docdb.js

--- test/docdb-spec.js
+++ test/docdb-spec.js
@@ -74,6 +74,138 @@
                 .catch(done);
             });
         });
+
+        it('queryUsers calls queryUsers method', done => {
+            docClient.prototype.queryUsers = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                var options = {};
+                service.queryUsers(mockQuery, options)
+                .then(() => {
+                    expect(docClient.prototype.queryUsers).toHaveBeenCalledWith('dbs/mydb', mockQuery, options);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('queryPartitionKeyRanges calls queryPartitionKeyRanges method', done => {
+            docClient.prototype.queryPartitionKeyRanges = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                var options = {};
+                service.queryPartitionKeyRanges(mockQuery, options)
+                .then(() => {
+                    expect(docClient.prototype.queryPartitionKeyRanges).toHaveBeenCalledWith('dbs/mydb', mockQuery, options);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('queryTriggers calls queryTriggers method', done => {
+            docClient.prototype.queryTriggers = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                var options = {};
+                service.queryTriggers(mockQuery, options)
+                .then(() => {
+                    expect(docClient.prototype.queryTriggers).toHaveBeenCalledWith('dbs/mydb/colls/mycoll', mockQuery, options);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('queryUserDefinedFunctions calls queryUserDefinedFunctions method', done => {
+            docClient.prototype.queryUserDefinedFunctions = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                var options = {};
+                service.queryUserDefinedFunctions(mockQuery, options)
+                .then(() => {
+                    expect(docClient.prototype.queryUserDefinedFunctions).toHaveBeenCalledWith('dbs/mydb/colls/mycoll', mockQuery, options);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('queryPermissions calls queryPermissions method', done => {
+            docClient.prototype.queryPermissions = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                var options = {};
+                service.queryPermissions('myuser', mockQuery, options)
+                .then(() => {
+                    expect(docClient.prototype.queryPermissions).toHaveBeenCalledWith('dbs/mydb/users/myuser', mockQuery, options);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('queryAttachments calls queryAttachments method', done => {
+            docClient.prototype.queryAttachments = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                var options = {};
+                service.queryAttachments('mydoc', mockQuery, options)
+                .then(() => {
+                    expect(docClient.prototype.queryAttachments).toHaveBeenCalledWith('dbs/mydb/colls/mycoll/docs/mydoc', mockQuery, options);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('queryStoredProcedures calls queryStoredProcedures method', done => {
+            docClient.prototype.queryStoredProcedures = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                service.queryStoredProcedures(mockQuery)
+                .then(() => {
+                    expect(docClient.prototype.queryStoredProcedures).toHaveBeenCalledWith('dbs/mydb/colls/mycoll', mockQuery);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('queryCollections calls queryCollections method', done => {
+            docClient.prototype.queryCollections = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                service.queryCollections(mockQuery)
+                .then(() => {
+                    expect(docClient.prototype.queryCollections).toHaveBeenCalledWith('dbs/mydb', mockQuery);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+
+        it('query calls queryDocuments method', done => {
+            docClient.prototype.queryDocuments = getMockFunction();
+
+            service.connect(mockConfig).then(()=>
+            {
+                service.query(mockQuery)
+                .then(() => {
+                    expect(docClient.prototype.queryDocuments).toHaveBeenCalledWith('dbs/mydb/colls/mycoll', mockQuery);
+                    done();
+                })
+                .catch(done);
+            });
+        });
     });
 
 } ());
